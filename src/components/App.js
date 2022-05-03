2f7d447f09351d787eeb6257e12a851e682bb5e4
Mario Andres Portilla <mportandres@gmail.com>
2022-05-03 15:07:52 -0400

    replaced components with elements, Added /add path

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Header from "./Header";
 import AddContact from './AddContact';
 import ContactList from './ContactList';
-
-
+import { v4 as uuid } from 'uuid';
+import { BrowserRouter as Router, Switch, Route, Routes} from "react-router-dom";
 
 
 function App() {
@@ -13,7 +13,15 @@ function App() {
 
     const addContactHandler = (contact) => {
         console.log(contact);
-        setContacts([...contacts, contact]);
+        setContacts([...contacts, {id: uuid(), ...contact}]);
+    };
+
+    const removeContactHandler = (id) => {
+        const newContactList = contacts.filter((contact) => {
+            return contact.id !== id;
+        });
+
+        setContacts(newContactList);
     };
 
     useEffect(() => {
@@ -22,15 +30,29 @@ function App() {
     },[]);
     
     useEffect(() => {
+        if (contacts.length>0){
         localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+        }
     }, [contacts]);
 
   return (
-      <div className="ui container">
-      <Header />
-      <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} />
+      <div className="ui container" >
 
+        <Router>
+          <Header />
+            <Routes>
+                <Route
+                    path="/"
+                    element={<ContactList/>} />
+                <Route
+                    path="/add"
+                    element={
+                    <AddContact/>} />
+            </Routes>
+
+          {/*<AddContact addContactHandler={addContactHandler} />*/}
+          {/*<ContactList contacts={contacts} getContactId={removeContactHandler} /> */}
+        </Router>
       </div>
   );
 }
